689599545dc71fee57490c3b65fa8270f5eb1e80
AugustoMachado01 <augustomachadochieno01@gmail.com>
2025-01-02 15:23:38 -0500

    enpoint delete task

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -15,4 +15,64 @@ module.exports = class TaskController {
     });
     res.status(201).json({ message: "create task success!", tasks: task });
   }
+
+  static async getAllUserTasks(req, res) {
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    const tasks = await prisma.task.findMany({ where: { userdId: user.id } });
+
+    res.status(200).json(tasks);
+  }
+
+  static async updateTask(req, res) {
+    const { id } = req.params;
+
+    const { title } = req.body;
+
+    const task = await prisma.task.findFirst({ where: { id: Number(id) } });
+
+    console.log("task==", task);
+
+    if (!task) {
+      res.status(404).json({ error: "Task not found" });
+      return;
+    }
+
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    if (task.userdId !== user.id) {
+      res.status(422).json({ error: "There was a problem, try later" });
+      return;
+    }
+
+    await prisma.task.update({ where: { id: Number(id) }, data: { title } });
+
+    res.status(200).json({ message: "task updated successfully" });
+  }
+
+  static async removeTaskById(req, res) {
+    const { id } = req.params;
+
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    // check if task exists
+    const task = await prisma.task.findFirst({ where: { id: Number(id) } });
+
+    if (!task) {
+      res.status(404).json({ error: "Task not found" });
+      return;
+    }
+
+    if (task.userdId !== user.id) {
+      res.status(422).json({ error: "There was a problem, try later" });
+      return;
+    }
+
+    await prisma.task.delete({ where: { id: Number(id) } });
+
+    res.status(200).send("delete success");
+  }
 };
--- src/routes/TaskRoute.js
+++ src/routes/TaskRoute.js
@@ -2,6 +2,24 @@ const router = require("express").Router();
 const TaskController = require("../controllers/TaskController");
 const widthAsyncErrorHandler = require("../middlaware/ErrorHandler");
 
+const verifyToken = require("../utils/verify-token");
+
 router.post("/create", widthAsyncErrorHandler(TaskController.register));
+router.get(
+  "/",
+  verifyToken,
+  widthAsyncErrorHandler(TaskController.getAllUserTasks)
+);
+router.put(
+  "/:id",
+  verifyToken,
+  widthAsyncErrorHandler(TaskController.updateTask)
+);
+
+router.delete(
+  "/:id",
+  verifyToken,
+  widthAsyncErrorHandler(TaskController.removeTaskById)
+);
 
 module.exports = router;
